e154932465584b33bf564e85da9de4101f625c03
Tushar Biswas <tusarbiswas888@gmail.com>
2026-02-01 03:25:07 +0600

    fix: fix params types  again

--- client/src/app/api/blogs/[slug]/reactions/[documentId]/route.ts
+++ client/src/app/api/blogs/[slug]/reactions/[documentId]/route.ts
@@ -52,7 +52,7 @@ export async function PATCH(
 
 export async function DELETE(
     request: NextRequest,
-    { params }: { params: { slug: string; documentId: string; }; }
+    { params }: { params: Promise<{ slug: string; documentId: string; }>; }
 ) {
     try {
         const { slug, documentId } = await params;
