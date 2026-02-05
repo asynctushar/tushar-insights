d572833ddad5bcdca606c15178df216a5571e25e
Tushar Biswas <tusarbiswas888@gmail.com>
2026-02-06 00:57:06 +0600

    hotfix: roleback to ssr of blog page

--- client/src/app/blogs/[slug]/page.tsx
+++ client/src/app/blogs/[slug]/page.tsx
@@ -72,8 +72,6 @@ export async function generateMetadata(
     };
 }
 
-export const dynamic = "force-static";
-
 
 const Blog = async ({ searchParams, params }: BlogProps) => {
     const { lang } = await searchParams;
