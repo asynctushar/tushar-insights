9fbdfa9183531e19517f8fbb8ec928bdf81906fd
Tushar Biswas <92681097+asynctushar@users.noreply.github.com>
2026-04-18 00:56:00 +0600

    Fix: Update allowed domain for uploads

--- client/next.config.ts
+++ client/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
         pathname: "/uploads/**",
       },
     ],
-    domains: ["tushar-insights.up.railway.app"],
+    domains: ["tushar-insights.up.railway-api.app"],
     unoptimized: true,
   }
 };
